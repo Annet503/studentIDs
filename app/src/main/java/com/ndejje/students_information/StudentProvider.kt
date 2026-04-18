05481427f34260fd633f345d8ca091719892de82
Annet503 <annetnakku77@gmail.com>
2026-04-18 12:47:10 +0300

    Initial commit

--- app/src/main/java/com/ndejje/students_information/StudentProvider.kt
+++ app/src/main/java/com/ndejje/students_information/StudentProvider.kt
@@ -12,10 +12,10 @@ data class Student(
 class StudentProvider {
     companion object {
         val studentList = listOf(
-            Student(1, "NAKKU ANNET", "24/2/314/D/01", "BIT", R.drawable.p1, true),
-            Student(2, "NANTONGO ZAINAH", "24/2/314/D/02", "BCS", R.drawable.p2, false),
-            Student(3, "NAMAZZI RITAH", "24/2/314/D/03", "BIT", R.drawable.p3, true),
-            Student(4, "KASEKENDE ROBERT", "24/2/314/D/04", "BSE", R.drawable.p4, true)
+            Student(1, "NAKKU ANNET", "24/2/314/D/011", "BIT", R.drawable.p1, true),
+            Student(2, "NANTONGO ZAINAH", "24/2/314/D/511", "BCS", R.drawable.p2, false),
+            Student(3, "NAMAZZI RITAH", "24/2/314/D/502", "BIT", R.drawable.p3, true),
+            Student(4, "KASEKENDE ROBERT", "24/2/314/D/023", "BSE", R.drawable.p4, true)
         )
     }
 }
